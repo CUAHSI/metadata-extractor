36ec0996413c68c84921c961dcdfcd91943005c6
sblack-usu <sblack@cuahsi.org>
2025-04-11 09:00:34 -0600

    reorder input parameters

--- hsextract/main.py
+++ hsextract/main.py
@@ -19,10 +19,10 @@ async def _extract(
 def extract(
     input_path: str,
     output_path: str,
+    retrieve_metadata_resource_id: Annotated[str, typer.Argument()] = None,
     input_base_url: Annotated[str, typer.Argument()] = "https://hydroshare.org/resource",
     output_base_url: Annotated[str, typer.Argument()] = "https://hydroshare.org/resource/extracted_metadata",
     user_metadata_filename: Annotated[str, typer.Argument()] = "hs_user_meta.json",
-    retrieve_metadata_resource_id: Annotated[str, typer.Argument()] = None,
 ):
     if retrieve_metadata_resource_id:
         adapter = HydroshareMetadataAdapter()
